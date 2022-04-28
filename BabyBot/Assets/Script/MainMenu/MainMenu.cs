4f9fe9add11f8d7a1425a1b759be1636b508ae2f
Kevin <k.delcroix@rubika-edu.com>
2022-04-28 11:27:09 +0200

    Add Base Menu
    For merge

---
+++ BabyBot/Assets/Script/MainMenu/MainMenu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject CreditMenu;
+    public GameObject OptionsMenu;
+    public TextMeshProUGUI NameInput;
+
+    public void Play()
+    {
+        SceneManagement.instance.LoadScene(1);
+    }
+    public void ReturnToMenu()
+    {
+        SceneManagement.instance.LoadScene(0);
+    }
+    public void Exit()
+    {
+        SceneManagement.instance.Quit();
+    }
+    public void Options()
+    {
+
+        CreditMenu.SetActive(false);
+        OptionsMenu.SetActive(true);
+    }
+    public void ExitOptions()
+    {
+        OptionsMenu.SetActive(false);
+    }
+    public void Credit()
+    {
+        OptionsMenu.SetActive(false);
+        CreditMenu.SetActive(true);
+    }
+    public void ExitCredit()
+    {
+        CreditMenu.SetActive(false);
+    }
+    public void ChangeName()
+    {
+        //if (NameInput.text != "") PlayerManager.Instance.PlayerName = NameInput.text;
+    }
+
+    
+}
