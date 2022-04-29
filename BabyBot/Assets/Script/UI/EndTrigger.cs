0a86615445e91fac20bc0ca46baf9d732a59f977
Guillaume.R <g.roge@rubika-edu.com>
2022-04-29 22:35:53 +0200

    End menu done

---
+++ BabyBot/Assets/Script/UI/EndTrigger.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class EndTrigger : MonoBehaviour
+{
+    private int numOfPlayer;
+    public GameObject endMenu;
+    public TextMeshProUGUI counterText;
+    public float cooldown;
+
+    private float actualTime;
+    private bool lockSysteme;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            numOfPlayer += 1;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            numOfPlayer -= 1;
+        }
+    }
+
+
+    private void Start()
+    {
+        actualTime = cooldown;
+        endMenu.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (numOfPlayer == 2)
+        {
+            lockSysteme = true;
+            endMenu.SetActive(true);
+
+            if (lockSysteme == false)
+            {
+                lockSysteme = true;
+                StartCoroutine(Wait());
+            }
+        }
+
+        if (lockSysteme == true)
+        {
+            actualTime -= Time.deltaTime;
+            counterText.text = Mathf.Round(actualTime).ToString();
+        }
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(cooldown);
+        SceneManager.LoadScene(0);
+    }
+}
