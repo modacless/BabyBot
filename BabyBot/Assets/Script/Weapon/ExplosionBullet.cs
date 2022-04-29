aeca4be3ff03d612fd94c886d6bf23251347e95b
Matis <m.duperray@rubika-edu.com>
2022-04-29 18:55:15 +0200

    Try Fix sfx

--- BabyBot/Assets/Script/Weapon/ExplosionBullet.cs
+++ BabyBot/Assets/Script/Weapon/ExplosionBullet.cs
@@ -6,17 +6,12 @@ public class ExplosionBullet : Bullet
 {
     public GameObject explosion;
 
-    public AudioSource waterImpactSource;
-
     protected override void OnDestroy()
     {
-        //Audio
-        AudioManager Audio = AudioManager.AMInstance;
-        float pitch = Random.Range(0.8f, 1.2f);
-        int index = Random.Range(0, (Audio.waterImpactsArray.Length - 1));
-        Audio.PlaySFX(Audio.waterImpactsArray[index], waterImpactSource, pitch);
-        //----
+        
         GameObject explos = Instantiate(explosion, transform.position, explosion.transform.rotation);
         explos.GetComponent<Bullet>().InitBullet(0, 0, 0, 0, Vector3.zero, fromPlayer);
+
+
     }
 }
